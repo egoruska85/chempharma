681ffc779f33e612c5cfdf6ecafc508c11071eff
= <=>
2023-02-18 23:37:22 +0500

    add product controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get 'home/index'
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "home#index"
   # Defines the root path route ("/")
