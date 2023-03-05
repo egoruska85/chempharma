35d961ed2ec9c40294c9e835c7f15e2c77debd37
= <=>
2023-03-05 12:48:18 +0500

    add category in products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :country
   belongs_to :manufacturer
+  belongs_to :category
   has_one_attached :image
   has_many_attached :images
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_225442) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_074305) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -70,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_225442) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["country_id"], name: "index_products_on_country_id"
     t.index ["manufacturer_id"], name: "index_products_on_manufacturer_id"
   end
@@ -90,6 +92,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_225442) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "countries"
   add_foreign_key "products", "manufacturers"
 end
