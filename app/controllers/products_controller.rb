33a7bc8c481afd5e05772cc3b0b84807bac5dae4
= <=>
2023-03-05 15:34:02 +0300

    work with product cotroller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    @product = Product.find(params[:id]) 
+  end
+  def create
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title_ru, :title_en, :desc_ru, :desc_en, :text, :country_id, :category_id, :manufacturer_id, :price, :image, :images => [])
+  end
 end
